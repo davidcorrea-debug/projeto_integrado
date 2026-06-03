70542ba23bb40e45cd017a54869c6551a6b571c5
Kaua10710 <kaduzerah06@gmail.com>
2026-06-03 11:56:47 -0300

    Fazendo testes das funcionalidades e banco de dados, melhorando o desing de algumas páginas, página de cliente ainda não finalizei o design.

--- test_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password = 'admin123';
-$hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 10]);
-echo "Hash: " . $hash . "\n";
-echo "Verifica: " . (password_verify($password, $hash) ? 'Sim' : 'Não') . "\n";
-?>
--- update_password.sql
+++
@@ -1,1 +0,0 @@
-UPDATE usuarios SET usuario_senha = '$2y$10$./6rupil/.AqzPUWJhyr0uJYKEoQ7/xu51JIwzMB4uUv7kKL5tHSq' WHERE usuario_email = 'admin@glowagenda.com';
